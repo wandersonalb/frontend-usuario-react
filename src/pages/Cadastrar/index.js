15e35e4c3186aeb5f7daf2682f01c661f1647435
Wands <wandersonalb@gmail.com>
2019-07-02 03:13:05 -0300

    Finaliza tela inicial, cadastro e listagem

--- src/pages/Cadastrar/index.js
+++ src/pages/Cadastrar/index.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import { MdHome } from 'react-icons/md';
+
 import './styles.css';
 
 export default class Cadastrar extends Component {
+    state = {
+        novoUsuario: '',
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        
+        const response = await api.post('usuario/', {
+            nome: this.state.novoUsuario,
+        });
+
+        this.props.history.push('listar/');
+    }
+
+    handleInputChange = (e) => {
+        this.setState({ novoUsuario: e.target.value });
+    }
+
+    toHome = () => {
+        this.props.history.push('/');
+    }
+
     render() {
         return (
             <div id="cadastrar-container">
-                <form action="">
-                    <input 
-                        placeholder="Nome do usuário"
-                    />
-                    <button type="submit">Criar</button>
-                </form>
+                <header>
+                    <a onClick={this.toHome}> 
+                        <MdHome size={25} color="#1b2b5c" />
+                    </a>
+                    <h1>Cadastrar Novo Usuário</h1>    
+                </header>
+                <div id="form-usuario">
+                    <form onSubmit={this.handleSubmit} >
+                        <input 
+                            placeholder="Nome do usuário"
+                            value={this.state.novoUsuario}
+                            onChange={this.handleInputChange}
+                        />
+                        <button type="submit">Criar</button>
+                    </form>
+                </div>
             </div>
         );
     }
--- src/pages/Listar/index.js
+++ src/pages/Listar/index.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
+import { MdHome } from 'react-icons/md';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 export default class Listar extends Component {
+    state = { 
+        usuarios: {},
+        query: '',
+    }
+    
+    async componentDidMount() {
+        const response = await api.get(`usuarios/`);
+
+        this.setState({ usuarios: response });
+    }
+
+    handleInputChange = (e) => {
+        this.setState({ query: e.target.value });
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        
+        const response = await api.get(`usuarios/?query=${this.state.query}`);
+        
+        this.setState({ usuarios: response })
+    }
+
+    toHome = () => {
+        this.props.history.push('/');
+    }
+
     render() {
         return (
-            <div id="main-container">
-                <h1>Usuários</h1>
+            <div id="listar-container">
+                <header>
+                    <a onClick={this.toHome}> 
+                        <MdHome size={25} color="#1b2b5c" />
+                    </a>
+                    <h1>Usuários</h1>    
+                </header>
+                <form onSubmit={this.handleSubmit} >
+                    <input 
+                        placeholder="Pesquisar"
+                        value={this.state.query}
+                        onChange={this.handleInputChange}
+                    />
+                </form>
+                <ul>
+                    { this.state.usuarios.data && this.state.usuarios.data.map(usuario => (
+                       <li key={usuario.id}>
+                           <strong>{usuario.nome}</strong>
+                       </li> 
+                    )) }   
+                </ul>                
             </div>
         );
     }
